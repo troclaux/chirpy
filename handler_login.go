bddaa89c4a8eb1202adb8ea05543c5d9f2a5889d
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-01-10 22:22:44 -0300

    feat: generate jwt after login

--- handler_login.go
+++ handler_login.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
@@ -17,8 +18,9 @@ func (cfg *apiConfig) handleLogin(w http.ResponseWriter, r *http.Request) {
 
 	// declare struct to store data
 	type Credential struct {
-		Password string `json:"password"`
-		Email    string `json:"email"`
+		Password           string `json:"password"`
+		Email              string `json:"email"`
+		Expires_in_seconds *int   `json:"expires_in_seconds,omitempty"`
 	}
 
 	credential := Credential{}
@@ -54,18 +56,42 @@ func (cfg *apiConfig) handleLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// get servers secret key used in the jwt's signature
+	jwtSigningKey := os.Getenv("SIGNING_KEY")
+
+	// Default expiration time is 1 hour
+	expirationSeconds := 3600
+	if credential.Expires_in_seconds != nil {
+		// If specified, use the minimum between requested time and max time
+		expirationSeconds = *credential.Expires_in_seconds
+	}
+
+	// Convert seconds to Duration (need to multiply by time.Second)
+	expirationDuration := time.Duration(expirationSeconds) * time.Second
+
+	tokenString, err := auth.MakeJWT(potentialUser.ID, jwtSigningKey, expirationDuration)
+	if err != nil {
+		log.Println("couldn't generate jwt")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// response struct
 	type UserWithoutPassword struct {
 		ID        uuid.UUID `json:"id"`
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
 		Email     string    `json:"email"`
+		Token     string    `json:"token"`
 	}
 
+	// set response values (user fields without password and with the jwt)
 	user := UserWithoutPassword{
 		ID:        potentialUser.ID,
 		CreatedAt: potentialUser.CreatedAt,
 		UpdatedAt: potentialUser.UpdatedAt,
 		Email:     potentialUser.Email,
+		Token:     tokenString,
 	}
 
 	// Set headers before writing response
