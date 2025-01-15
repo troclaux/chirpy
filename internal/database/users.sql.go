c2556ff30221127d44b5ed2a72faab966d940ab4
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-01-15 14:02:37 -0300

    feat: add sql query to update user with new email and password

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -7,12 +7,14 @@ package database
 
 import (
 	"context"
+
+	"github.com/google/uuid"
 )
 
 const authenticateUser = `-- name: AuthenticateUser :one
 SELECT id, created_at, updated_at, email, hashed_password
 FROM users
-WHERE email=$1
+WHERE email = $1
 LIMIT 1
 `
 
@@ -42,6 +44,32 @@ type CreateUserParams struct {
 
 func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
 	row := q.db.QueryRowContext(ctx, createUser, arg.Email, arg.HashedPassword)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Email,
+		&i.HashedPassword,
+	)
+	return i, err
+}
+
+const updateUser = `-- name: UpdateUser :one
+UPDATE users
+SET email = $1, hashed_password = $2, updated_at = NOW()
+WHERE id = $3
+RETURNING id, created_at, updated_at, email, hashed_password
+`
+
+type UpdateUserParams struct {
+	Email          string
+	HashedPassword string
+	ID             uuid.UUID
+}
+
+func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, updateUser, arg.Email, arg.HashedPassword, arg.ID)
 	var i User
 	err := row.Scan(
 		&i.ID,
